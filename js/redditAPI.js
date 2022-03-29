d322fbb9731042a5ca8578fcec712759ef64c36d
tarunsahnan <tarunsahnan98@gmail.com>
2022-03-30 00:16:53 +0530

    improvement in HTML CSS

---
+++ js/redditAPI.js
@@ -0,0 +1,10 @@
+export default{
+
+    fetchResult: function(key,sort,limit){
+
+        // fetch(`https://www.reddit.com/search/?q=${key}`)
+        //     .then((res) => console.log(data.json));
+console.log(10);
+    }
+
+}
--- js/script.js
+++ js/script.js
@@ -1,2 +1,36 @@
+import reddit from './redditAPI.js';
 
 
+function search(e){
+    e.preventDefault();
+
+    let key = document.querySelector('.searchText input').value;
+    let sort = document.querySelector('input[name="radio"]:checked').value;
+    let  limit= document.querySelector('select').value;
+    
+    if(key == "")
+        showMsg("Please enter the Keyword","error");
+    else{
+    reddit.fetchResult(key,sort,limit);
+
+
+    }
+}
+
+
+function showMsg(msg,cls){
+    let div = document.createElement('div');
+    div.className=`alert ${cls}`;
+    div.appendChild(document.createTextNode(msg));
+
+    body.insertBefore(div,searchArea);
+    console.log(div.classList);
+    setTimeout(function(){
+        document.querySelector('.alert').remove();
+    },3000);
+}
+
+document.querySelector(".btn").addEventListener('click',search);
+let searchArea = document.querySelector(".searchArea");
+let form = document.querySelector('form');
+let body = document.querySelector('body');
