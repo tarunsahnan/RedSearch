7f5a2d83c61b250c1fa8e21f15687c23d9141b5a
tarunsahnan <tarunsahnan98@gmail.com>
2022-04-03 01:20:00 +0530

    major update

--- js/script.js
+++ js/script.js
@@ -1,36 +1,10 @@
-import reddit from './redditAPI.js';
 
 
-function search(e){
-    e.preventDefault();
-
-    let key = document.querySelector('.searchText input').value;
-    let sort = document.querySelector('input[name="radio"]:checked').value;
-    let  limit= document.querySelector('select').value;
-    
-    if(key == "")
-        showMsg("Please enter the Keyword","error");
-    else{
-    reddit.fetchResult(key,sort,limit);
+document.querySelector(`.cmtSubRadio inputname="detailed":checked`).addEventListener("click",displayFilters);
 
 
-    }
+
+function displayFilters(e){
+    console.log(10);
 }
-
-
-function showMsg(msg,cls){
-    let div = document.createElement('div');
-    div.className=`alert ${cls}`;
-    div.appendChild(document.createTextNode(msg));
-
-    body.insertBefore(div,searchArea);
-    console.log(div.classList);
-    setTimeout(function(){
-        document.querySelector('.alert').remove();
-    },3000);
-}
-
-document.querySelector(".btn").addEventListener('click',search);
-let searchArea = document.querySelector(".searchArea");
-let form = document.querySelector('form');
-let body = document.querySelector('body');
+// detailedSearchRB.style.display="grid";
