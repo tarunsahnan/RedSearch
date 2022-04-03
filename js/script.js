9522d3864cc13f9fd6e3c3efa47adb7d00546a78
tarunsahnan <tarunsahnan98@gmail.com>
2022-04-03 22:33:01 +0530

    default search completed

--- js/script.js
+++ js/script.js
@@ -1,41 +1,115 @@
 
-
-document.querySelector('.searchButton').addEventListener('click',search);
-document.querySelector('.detailedSearch').addEventListener('click',display);
-// if(document.querySelector('input[name="detailed"]:checked')){
-//     document.querySelector('input[name="detailed"]:checked').addEventListener('click',display);
-
-// }
-// function display(e){
-//     e.preventDefault();
-//     console.log(e);
-// }
-
-async function search(e){
-    e.preventDefault();
-
-    // default search no radio button seleced
-    if(document.querySelector('input[name="detailed"]:checked') == null){
-            console.log(10)
-    }
-}
-
+//show comment/submission form 
 function display(e){
     e.preventDefault();
-    const val= document.querySelector('.detailedSearch').value;
+    const val= detailedSearch.value;
 
     if(val == 'submission'){
-        document.querySelector('.submission').style.display = "grid";
-        document.querySelector('.comment').style.display = "none";
+        submission.style.display = "grid";
+        comment.style.display = "none";
     }
     else if(val == 'comment'){
-        
-        document.querySelector('.comment').style.display = "grid";
-        document.querySelector('.submission').style.display = "none";
-        
+        comment.style.display = "grid";
+        submission.style.display = "none";   
+    }
+    else{
+        submission.style.display = "none";
+        comment.style.display = "none";
     }
 
 }
-function displaySubmission(){
+
+//display alerts for 3 seconds
+function showAlert(msg,cls){
+
+    const tempAlert=document.createElement('div');
+    tempAlert.appendChild(document.createTextNode(msg+'!'));
+    tempAlert.className = `alert alert-${cls}`;
+    const body = document.querySelector("body");
+    const Area = document.querySelector('.searchArea');
+    body.insertBefore(tempAlert, Area);
+
+    setTimeout(()=>{
+        document.querySelector('.alert').remove()
+    },3000);
 
 }
+
+function search(e){
+    e.preventDefault();
+
+    if(searchKey.value==""){
+        showAlert("Cannot Search Empty List",'danger');
+    }
+    else{
+
+        showAlert("Searching... Please Wait","info");
+
+        const val= detailedSearch.value;
+
+        searchButton.disabled=true;
+
+        if(val == 'none')
+            defaultSearch();
+        else if(val == 'submission')
+            submissionSearch();
+        else
+            commentSearch();
+    }
+}
+
+async function defaultSearch(){
+
+    console.log(searchKey.value);
+    await (fetch(`https://api.pushshift.io/reddit/search/submission/?q=${searchKey.value}&over_18="false"`)
+                .then(res => res.json())
+                    .then(res => {
+                        
+                        console.log(res.data);
+                        let output="<div class='card-columns cardGrid'>";
+                        res.data.forEach(item => {
+
+                            output += `
+                            <div class="card mb-3">
+                            <div>
+                                <div>
+                                <div class="card-body">
+                                    <a href="${item.full_link}" class="noHover">
+                                    <h5 class="card-title">${item.title}</h5>
+                                    </a>
+                                    
+                                    <p class="card-text"><small class="text-muted">${truncate(item.selftext,200)}</small></p>
+                                    <a class="noHover" href="https://www.reddit.com/user/${item.author}"<p class="card-text"><i class="fa-solid fa-user "></i> ${item.author}</p></a>
+                                    <p class="card-text line"><i class="fa-solid fa-comment"></i> ${item.num_comments}</p>
+                                </div>
+                                </div>
+                            </div>
+                            </div>`;
+                        });
+                        output+='</div>';
+                        document.querySelector(".result").innerHTML = output;
+                        console.log(document.querySelector(".result"));
+                    }));
+
+
+    searchButton.disabled=false;
+}
+
+function truncate(str,limit){
+    const index= str.indexOf(' ',limit);
+    if(index == -1) return str;
+    return str.substring(0,index);
+}
+
+const searchButton = document.querySelector('.searchButton');
+searchButton.addEventListener('click',search);
+
+const detailedSearch = document.querySelector('.detailedSearch');
+detailedSearch.addEventListener('click',display);
+
+const searchKey = document.querySelector("input[name='default']");
+
+const submission=document.querySelector('.submission');
+const comment=document.querySelector('.comment');
+
+
