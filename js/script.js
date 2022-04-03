7e452f5e0b2037b162397b126defb6c6b2d92ef2
tarunsahnan <tarunsahnan98@gmail.com>
2022-04-04 01:38:21 +0530

    All functionalities done

--- js/script.js
+++ js/script.js
@@ -19,6 +19,9 @@ function display(e){
         comment.style.display = "none";
     }
 
+    pushAPI.style.display="none";
+
+
 }
 
 //display alerts for 3 seconds
@@ -41,6 +44,8 @@ function showAlert(msg,cls){
 function search(e){
     e.preventDefault();
 
+    pushAPI.style.display="none";
+
     const val= detailedSearch.value;
 
     searchButton.disabled=true;
@@ -56,20 +61,25 @@ function search(e){
     else{
         if(searchKey.value==""){
             showAlert("Cannot Search Empty List",'danger');
+            searchButton.disabled=false;
         }
-        else
-        defaultSearch();;
+        else{
+            showAlert("Searching... Please Wait","info");
+            defaultSearch();;
+        }
     }
 }
 
 //display result when none is selected
 function defaultSearch(){
-    console.log(searchKey.value);
   fetch(`https://api.pushshift.io/reddit/search/submission/?q=${searchKey.value}&over_18=false`)
                 .then(res => res.json())
                     .then(res => {
                         
-                        console.log(res.data);
+                        if(res.data.length == 0){
+                            showAlert("No Results Found","danger");
+                        }
+    
                         let output="<div class='card-columns cardGrid'>";
                         res.data.forEach(item => {
                             output += `
@@ -90,7 +100,6 @@ function defaultSearch(){
                         });
                         output+='</div>';
                         result.innerHTML = output;
-                        console.log(document.querySelector(".result"));
                     });
 
     searchButton.disabled=false;
@@ -98,56 +107,64 @@ function defaultSearch(){
 
 //generated url for submission search. It is helper function of function at line 157
 function generateSubmissionUrl(urlStr){
-    if(document.querySelector(`input[name="ids"]`).value != ""){
-        urlStr += "&ids=" + document.querySelector(`input[name="ids"]`).value;
+
+
+    if(document.querySelector(`.submission input[name="ids"]`).value != ""){
+        const ids= document.querySelector(`.submission input[name="ids"]`).value;
+
+        if((ids.indexOf('_') != -1) || (ids.indexOf('@') != -1) || (ids.indexOf('#') != -1)){
+            showAlert("ID ignored: ID can't have special character","danger");
+        }
+
+        urlStr += "&ids=" + ids;
     }
 
-    if(document.querySelector(`.author input`).value != ""){
-        urlStr += "&author=" + document.querySelector(`.author input`).value;
+    if(document.querySelector(`.submission .author input`).value != ""){
+        urlStr += "&author=" + document.querySelector(`.submission .author input`).value;
     }
 
-    if(document.querySelector(`.subreddit input`).value != ""){
-        urlStr += "&subreddit=" + document.querySelector(`.subreddit input`).value;
+    if(document.querySelector(`.submission .subreddit input`).value != ""){
+        urlStr += "&subreddit=" + document.querySelector(`.submission .subreddit input`).value;
     }
 
-    if(document.querySelector(`.q-not input`).value != ""){
-        urlStr += "&q:not=" + document.querySelector(`.q-not input`).value;
+    if(document.querySelector(`.submission .q-not input`).value != ""){
+        urlStr += "&q:not=" + document.querySelector(`.submission .q-not input`).value;
     }
 
-    if(document.querySelector(`.title-yes input`).value != ""){
-        urlStr += "&title=" + document.querySelector(`.title-yes input`).value;
+    if(document.querySelector(`.submission .title-yes input`).value != ""){
+        urlStr += "&title=" + document.querySelector(`.submission .title-yes input`).value;
     }
 
-    if(document.querySelector(`.title-not input`).value != ""){
-        urlStr += "&title:not=" + document.querySelector(`.title-not input`).value;
+    if(document.querySelector(`.submission .title-not input`).value != ""){
+        urlStr += "&title:not=" + document.querySelector(`.submission .title-not input`).value;
     }
 
-    if(document.querySelector(`input[name="sort"]:checked`).value != ""){
-        urlStr += "&sort=" + document.querySelector(`input[name="sort"]:checked`).value;
+    if(document.querySelector(`input[name="sub-sort"]:checked`).value != ""){
+        urlStr += "&sort=" + document.querySelector(`input[name="sub-sort"]:checked`).value;
     }
 
-    if(document.querySelector(`.sort-type select`).value != ""){
-        urlStr += "&sort_type=" + document.querySelector(`.sort-type select`).value;
+    if(document.querySelector(`.submission .sort-type select`).value != ""){
+        urlStr += "&sort_type=" + document.querySelector(`.submission .sort-type select`).value;
     }
 
-    if(document.querySelector(`.score input`).value != ""){
-        urlStr += "&score=" + document.querySelector(`.score input`).value;
+    if(document.querySelector(`.submission .score input`).value != ""){
+        urlStr += "&score=" + document.querySelector(`.submission .score input`).value;
     }
 
-    if(document.querySelector(`.num_comments input`).value != ""){
-        urlStr += "&num_comments=" + document.querySelector(`.num_comments input`).value;
+    if(document.querySelector(`.submission .num_comments input`).value != ""){
+        urlStr += "&num_comments=" + document.querySelector(`.submission .num_comments input`).value;
     }
 
-    if(document.querySelector(`.size input`).value != ""){
-        urlStr += "&size=" + document.querySelector(`.size input`).value;
+    if(document.querySelector(`.submission .size input`).value != ""){
+        urlStr += "&size=" + document.querySelector(`.submission .size input`).value;
     }
 
-    if(document.querySelector(`.after input`).value != ""){
-        urlStr += "&after=" + document.querySelector(`.after input`).value;
+    if(document.querySelector(`.submission .after input`).value != ""){
+        urlStr += "&after=" + document.querySelector(`.submission .after input`).value;
     }
 
-    if(document.querySelector(`.before input`).value != ""){
-        urlStr += "&before=" + document.querySelector(`.before input`).value;
+    if(document.querySelector(`.submission .before input`).value != ""){
+        urlStr += "&before=" + document.querySelector(`.submission .before input`).value;
     }
 
     return urlStr;
@@ -158,13 +175,92 @@ function submissionSearch(){
 
     let urlStr=generateSubmissionUrl(searchKey.value);
     
-    console.log(urlStr);
+
     fetch(`https://api.pushshift.io/reddit/search/submission/?q=${urlStr}&over_18=false`)
         .then(res =>res.json())
             .then(res => {
-                console.log(res.data);
+                
+                if(res.data.length == 0){
+                    showAlert("No Results Found","danger");
+                }
+                let output="<div class='card-columns cardGrid'>";
+                res.data.forEach(item => {
+                    output += `
+                    <div class="card mb-3">
+                    <div>
+                        <div>
+                        <div class="card-body">
+                            <a target="_blank" href="${item.full_link}" class="noHover">
+                            <h5 class="card-title">${item.title}</h5>
+                            </a>
+                            <p class="card-text"><small class="text-muted">${truncate(item.selftext,200)}</small></p>
+                            <a class="noHover" target="_blank" href="https://www.reddit.com/user/${item.author}"<p class="card-text"><i class="fa-solid fa-user "></i> ${item.author}</p></a>
+                            <p class="card-text line"><i class="fa-solid fa-comment"></i> ${item.num_comments}</p>
+                        </div>
+                        </div>
+                    </div>
+                    </div>`;
+                });
+                output+='</div>';
+                result.innerHTML = output;
+            });
 
-                console.log(res.data);
+    searchButton.disabled=false;
+}
+
+//generate url from commentSearch. Helper of function @line 251
+function generateCommentUrl(urlStr){
+    if(document.querySelector(`.comment input[name="ids"]`).value != ""){
+        urlStr += "&ids=" + document.querySelector(`.comment input[name="ids"]`).value;
+    }
+
+    if(document.querySelector(`.comment .author input`).value != ""){
+        urlStr += "&author=" + document.querySelector(`.comment .author input`).value;
+    }
+
+    if(document.querySelector(`.comment .subreddit input`).value != ""){
+        urlStr += "&subreddit=" + document.querySelector(`.comment .subreddit input`).value;
+    }
+
+   
+    if(document.querySelector(`input[name="comm-sort"]:checked`).value != ""){
+        urlStr += "&sort=" + document.querySelector(`input[name="comm-sort"]:checked`).value;
+    }
+
+    if(document.querySelector(`.comment .sort-type select`).value != ""){
+        urlStr += "&sort_type=" + document.querySelector(`.comment .sort-type select`).value;
+    }
+
+
+    if(document.querySelector(`.comment .size input`).value != ""){
+        urlStr += "&size=" + document.querySelector(`.comment .size input`).value;
+    }
+
+    if(document.querySelector(`.comment .after input`).value != ""){
+        urlStr += "&after=" + document.querySelector(`.comment .after input`).value;
+    }
+
+    if(document.querySelector(`.comment .before input`).value != ""){
+        urlStr += "&before=" + document.querySelector(`.comment .before input`).value;
+    }
+
+    return urlStr;
+}
+
+//display result when comment is selected
+function commentSearch(){
+    
+    let urlStr=generateCommentUrl(searchKey.value);
+
+    fetch(`https://api.pushshift.io/reddit/search/comment/?q=${urlStr}`)
+        .then(res =>res.json())
+            .then(res => {
+
+
+                    if(res.data.length == 0){
+                        showAlert("No Results Found","danger");
+                    }
+
                         let output="<div class='card-columns cardGrid'>";
                         res.data.forEach(item => {
                             output += `
@@ -172,12 +268,9 @@ function submissionSearch(){
                             <div>
                                 <div>
                                 <div class="card-body">
-                                    <a target="_blank" href="${item.full_link}" class="noHover">
-                                    <h5 class="card-title">${item.title}</h5>
-                                    </a>
-                                    <p class="card-text"><small class="text-muted">${truncate(item.selftext,200)}</small></p>
+                                    <h5 class="card-title">${item.subreddit}</h5>
+                                    <p class="card-text"><small class="text-muted">${truncate(item.body,200)}</small></p>
                                     <a class="noHover" target="_blank" href="https://www.reddit.com/user/${item.author}"<p class="card-text"><i class="fa-solid fa-user "></i> ${item.author}</p></a>
-                                    <p class="card-text line"><i class="fa-solid fa-comment"></i> ${item.num_comments}</p>
                                 </div>
                                 </div>
                             </div>
@@ -189,6 +282,7 @@ function submissionSearch(){
 
     searchButton.disabled=false;
 }
+
 
 //truncate big strings to small length
 function truncate(str,limit){
@@ -202,6 +296,8 @@ searchButton.addEventListener('click',search);
 
 const detailedSearch = document.querySelector('.detailedSearch');
 detailedSearch.addEventListener('click',display);
+
+const pushAPI= document.querySelector(".pushshiftAPI");
 
 const searchKey = document.querySelector("input[name='default']");
 const result =document.querySelector(".result");
